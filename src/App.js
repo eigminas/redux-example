5694437b78cad21bad47f2e8de52e18d68122508
Eigminas Slavinskas <e.slavinskas@newcastle.ac.uk>
2020-07-11 12:44:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+      <PostForm />
+      <hr />
+      <Posts />
+      </div>
+    </Provider>
+    
+  );
+}
+
+export default App;
